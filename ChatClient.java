b48c2eddab3db98ace69df3d91508cd33d934f6a
Seannnnnnnnnnn <47133490+Seannnnnnnnnnn@users.noreply.github.com>
2021-08-28 21:22:38 +1000

    Add files via upload

--- ChatClient.java
+++ ChatClient.java
@@ -1,12 +1,12 @@
 package com.ChatRoomApplication;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class ChatClient {
     private static final int serverPort = 4444;
-    private static String proxy = "localhost";
-    private static String name;
+    private static final String location = "localhost";
+    private static final boolean connectionAlive = true;
 
     public static void main(String[] args) {
         establishConnection();
@@ -14,9 +14,77 @@ public class ChatClient {
 
     public static void establishConnection() {
         try {
-            Socket socket = new Socket(proxy, serverPort);
+            Socket socket = new Socket(location, serverPort);
+
+            Thread listener = new Listener(socket);
+            Thread writer = new Writer(socket);
+            listener.start();
+            writer.start();
+
         } catch (IOException e) {
             e.getStackTrace();
         }
     }
+
+
+    private static class Writer extends Thread {
+        /*
+        Thread for handling the writing and flushing of client messages
+        */
+
+        PrintWriter writer;
+        BufferedReader keyboardReader = new BufferedReader(new InputStreamReader(System.in));
+
+        public Writer(Socket socket) {
+            try {
+                this.writer = new PrintWriter(socket.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            while (connectionAlive) {
+                try {
+                    String clientInput = keyboardReader.readLine();
+                    writer.println(clientInput);
+                    writer.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    private static class Listener extends Thread {
+        /*
+        Thread for handling the reading of messages from server
+        */
+        BufferedReader reader;
+
+        public Listener(Socket socket) {
+            try {
+                this.reader =  new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            while (connectionAlive) {
+                try {
+                    String input = reader.readLine();
+                    System.out.println(input);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
 }
